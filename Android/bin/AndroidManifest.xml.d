b8ded07840085cac17bfb3b296b792e5b9663608
cjgammon <cj.gammon@gmail.com>
2013-08-01 12:13:00 -0700

    remove bin folder

--- Android/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/gammon/Documents/Projects/00-repos/git.corp.adobe/demo-fastcanvas/fastcanvas/Android/bin/AndroidManifest.xml : \
-/Users/gammon/Documents/Projects/00-repos/git.corp.adobe/demo-fastcanvas/fastcanvas/Android/AndroidManifest.xml \
